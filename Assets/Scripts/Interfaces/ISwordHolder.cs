223cdd881b7140b8701ac99de8d215788991f123
SerhanT <serhanturgul2@gmail.com>
2023-06-30 11:20:14 +0300

    Update ISwordHolder.cs

--- Assets/Scripts/Interfaces/ISwordHolder.cs
+++ Assets/Scripts/Interfaces/ISwordHolder.cs
@@ -1,18 +1,5 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class ISwordHolder : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+public interface ISwordHolder{
+    public Sword Sword{get;set;}
+    void AcquireSword(Sword swordToAcquire);
+    void DropSword();
 }
